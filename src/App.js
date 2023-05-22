4fa40c002d3a9ef8d98bc5ee9b3d3df5b552f39e
nickAmos <132029265+nickAmos@users.noreply.github.com>
2023-05-22 18:19:10 +1000

    Generate mons merge
    Moved individual generate buttons into a single button found in the App.js component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 
 import './App.css';
 import Pokemon from './Pokemon';
+import { useEffect, useState } from "react";
 
 
 
@@ -8,11 +9,157 @@ import Pokemon from './Pokemon';
 
 function App() {
 
+  const [pokemonOne, setPokemonOne] = useState({
+    name: '',
+    type: '',
+    secondType: ''
+});
+
+const [pokemonTwo, setPokemonTwo] = useState({
+  name: '',
+  type: '',
+  secondType: ''
+});
+
+const [pokemonThree, setPokemonThree] = useState({
+  name: '',
+  type: '',
+  secondType: ''
+});
+
+
+const [numOne, setNumOne] = useState(1);
+const [numTwo, setNumTwo] = useState(1);
+const [numThree, setNumThree] = useState(1);
+
+
+function generateNumbers() {
+    const randomNumberOne = Math.floor(Math.random() * 1010);
+    const randomNumberTwo = Math.floor(Math.random() * 1010);
+    const randomNumberThree = Math.floor(Math.random() * 1010);
+    
+    setNumOne(randomNumberOne);
+    setNumTwo(randomNumberTwo);
+    setNumThree(randomNumberThree);
+}
+
+
+
+useEffect(()=> {
+    async function fetchData() {
+        const url = 'https://pokeapi.co/api/v2/pokemon/'
+        const endpoint = numOne
+        const urlToFetch = `${url}${endpoint}`
+        try {
+          const response = await fetch(urlToFetch);
+          const json = await response.json();
+          if (json.types.length === 1) {
+            const poke = {
+                name: json.name,
+                type: json.types[0].type.name,
+                image: json.sprites.front_default
+              }
+              setPokemonOne(poke);
+          } else {
+            const poke = {
+                name: json.name,
+                type: json.types[0].type.name,
+                typeTwo: json.types[1].type.name,
+                image: json.sprites.front_default
+              }
+              setPokemonOne(poke);
+          }
+        
+          
+          
+        } catch (error) {
+          console.error(error);
+        }
+      }
+      fetchData();
+    }, [numOne]);
+
+    useEffect(()=> {
+      async function fetchData() {
+          const url = 'https://pokeapi.co/api/v2/pokemon/'
+          const endpoint = numTwo
+          const urlToFetch = `${url}${endpoint}`
+          try {
+            const response = await fetch(urlToFetch);
+            const json = await response.json();
+            if (json.types.length === 1) {
+              const poke = {
+                  name: json.name,
+                  type: json.types[0].type.name,
+                  image: json.sprites.front_default
+                }
+                setPokemonTwo(poke);
+            } else {
+              const poke = {
+                  name: json.name,
+                  type: json.types[0].type.name,
+                  typeTwo: json.types[1].type.name,
+                  image: json.sprites.front_default
+                }
+                setPokemonTwo(poke);
+            }
+          
+            
+            
+          } catch (error) {
+            console.error(error);
+          }
+        }
+        fetchData();
+      }, [numTwo]);
+
+      useEffect(()=> {
+        async function fetchData() {
+            const url = 'https://pokeapi.co/api/v2/pokemon/'
+            const endpoint = numThree
+            const urlToFetch = `${url}${endpoint}`
+            try {
+              const response = await fetch(urlToFetch);
+              const json = await response.json();
+              if (json.types.length === 1) {
+                const poke = {
+                    name: json.name,
+                    type: json.types[0].type.name,
+                    image: json.sprites.front_default
+                  }
+                  setPokemonThree(poke);
+              } else {
+                const poke = {
+                    name: json.name,
+                    type: json.types[0].type.name,
+                    typeTwo: json.types[1].type.name,
+                    image: json.sprites.front_default
+                  }
+                  setPokemonThree(poke);
+              }
+            
+              
+              
+            } catch (error) {
+              console.error(error);
+            }
+          }
+          fetchData();
+        }, [numOne]);
+ 
+
+
   return (
-    <div className='Container'>
-      <Pokemon className='Pokemon' />    
+    <>
+    <button className='button-54' onClick={generateNumbers}>Generate new pokemon</button>
+    <div className='App-Container'>
+      <Pokemon className='Pokemon' name={pokemonOne.name} src={pokemonOne.image} typeOne={pokemonOne.type} typeTwo={pokemonOne.typeTwo} />   
+      <Pokemon className='Pokemon' name={pokemonTwo.name} src={pokemonTwo.image} typeOne={pokemonTwo.type} typeTwo={pokemonTwo.typeTwo}/> 
+      <Pokemon className='Pokemon' name={pokemonThree.name} src={pokemonThree.image} typeOne={pokemonThree.type} typeTwo={pokemonThree.typeTwo}/>
+      
       
     </div>
+    </>
   );
 }
 
--- src/Pokemon.js
+++ src/Pokemon.js
@@ -1,65 +1,14 @@
-import { useEffect, useState } from "react";
 import './Pokemon.css';
 
-function Pokemon() {
+function Pokemon(props) {
 
-    const [pokemon, setPokemon] = useState({
-        name: '',
-        type: '',
-        secondType: ''
-    });
-
-    const [num, setNum] = useState(1);
-
-    function generateNum() {
-        const randomNumber = Math.floor(Math.random() * 1010);
-        console.log(randomNumber);
-        setNum(randomNumber);
-    }
-
-    
-
-    useEffect(()=> {
-        async function fetchData() {
-            const url = 'https://pokeapi.co/api/v2/pokemon/'
-            const endpoint = num
-            const urlToFetch = `${url}${endpoint}`
-            try {
-              const response = await fetch(urlToFetch);
-              const json = await response.json();
-              if (json.types.length === 1) {
-                const poke = {
-                    name: json.name,
-                    type: json.types[0].type.name,
-                    image: json.sprites.front_default
-                  }
-                  setPokemon(poke);
-              } else {
-                const poke = {
-                    name: json.name,
-                    type: json.types[0].type.name,
-                    typeTwo: json.types[1].type.name,
-                    image: json.sprites.front_default
-                  }
-                  setPokemon(poke);
-              }
-            
-              
-              
-            } catch (error) {
-              console.error(error);
-            }
-          }
-          fetchData();
-        }, [num]);
 
     return (
         <div className="Container">
-            <button className='button-54' onClick={generateNum}>Generate new pokemon</button>
             <div className="pokeInfo">
-                <h1>{pokemon.name}</h1>
-                <img src={pokemon.image} alt='pokemonSprite'/>
-                <p>{pokemon.type} {pokemon.typeTwo}</p>
+                <h1>{props.name}</h1>
+                <img  src={props.src} alt='pokemonSprite'/>
+                <p>{props.typeOne} {props.typeTwo}</p>
             </div>
         </div>
     )
